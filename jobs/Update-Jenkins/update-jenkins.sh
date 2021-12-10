65711e7c1c124164b0aeeae1fdba06623320764b
Mike Fugate <mfugate@carnegielearning.com>
2021-12-09 22:00:09 -0500

    fix update script

--- jobs/Update-Jenkins/update-jenkins.sh
+++ jobs/Update-Jenkins/update-jenkins.sh
@@ -1,5 +1,10 @@
+cat << EOF > update-jenkins.sh
 cd /docker/server-management/truenas-docker
 git pull
 docker-compose pull
 sleep 5
 docker-compose up -d --force-recreate
+EOF
+chmod +x update-jenkins.sh
+scp -o StrictHostKeyChecking=no update-jenkins.sh jenkins@192.168.1.172:~/
+ssh -o StrictHostKeyChecking=no jenkins@192.168.1.172 'nohup ./update-jenkins.sh > /dev/null 2>&1 &'
