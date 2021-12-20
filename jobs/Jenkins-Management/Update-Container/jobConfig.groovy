9a8a7b08ff4aa783af929b11d315c992efb55422
Mike Fugate <mfugate@carnegielearning.com>
2021-12-19 20:52:54 -0500

    use azure vault for all secrets

--- jobs/Jenkins-Management/Update-Container/jobConfig.groovy
+++ jobs/Jenkins-Management/Update-Container/jobConfig.groovy
@@ -9,6 +9,70 @@ job ('Jenkins-Update-Container') {
     }
     wrappers {
         sshAgent('docker1-ssh')
+        withAzureKeyvault {
+            azureKeyVaultSecrets {
+                azureKeyVaultSecret {
+                    envVariable('AZURE-CLIENT-ID')
+                    name('AZURE_CLIENT_ID')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('AZURE-CLIENT-SECRET')
+                    name('AZURE_CLIENT_SECRET')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('AZURE-SUBSCRIPTION-ID')
+                    name('AZURE_SUBSCRIPTION_ID')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('AZURE-TENANT')
+                    name('AZURE_TENANT')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('DOCKER1-SSH-PRIVATE-KEY')
+                    name('DOCKER1_SSH_PRIVATE_KEY')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('GARMIN-CONNECT-PASSWORD')
+                    name('GARMIN_CONNECT_PASSWORD')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('GARMIN-CONNECT-USERNAME')
+                    name('GARMIN_CONNECT_USERNAME')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('JENKINS-ADMIN-EMAIL')
+                    name('JENKINS_ADMIN_EMAIL')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('JENKINS-IP')
+                    name('JENKINS_IP')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('JENKINS-PASSWORD')
+                    name('JENKINS_PASSWORD')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('JENKINS-PORT')
+                    name('JENKINS_PORT')
+                    secretType('Secret')
+                }
+                azureKeyVaultSecret {
+                    envVariable('JENKINS-TRUENAS-DOCKER-UPDATE-TOKEN')
+                    name('JENKINS_TRUENAS_DOCKER_UPDATE_TOKEN')
+                    secretType('Secret')
+                }
+            }
+        }
     }
     steps {
         shell(readFileFromWorkspace('jobs/Jenkins-Management/Update-Container/update-jenkins.sh'))
