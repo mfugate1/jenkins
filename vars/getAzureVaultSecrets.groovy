a08306330d348e7be2f0d277c7a3de2c5b887e4e
Mike Fugate <mfugate@carnegielearning.com>
2021-12-21 18:51:15 -0500

    add global shared library and a function to get azure vault secrets

---
+++ vars/getAzureVaultSecrets.groovy
@@ -0,0 +1,7 @@
+@NonCPS
+Map call() {
+    return com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        org.jenkinsci.plugins.azurekeyvaultplugin.credentials.string.AzureSecretStringCredentials,
+        Jenkins.instance
+    ).collectEntries{[("\${${it.id}}"): it.secret]}
+}
