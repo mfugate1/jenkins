554b0d760861c17cc949cd9503197bba47aa04bc
Mike Fugate <mfugate@carnegielearning.com>
2021-12-22 20:38:30 -0500

    add username password secrets from vault as two separate secrets

--- vars/getAzureVaultSecrets.groovy
+++ vars/getAzureVaultSecrets.groovy
@@ -1,7 +1,18 @@
 @NonCPS
 Map call() {
-    return com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+    Map secrets = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
         org.jenkinsci.plugins.azurekeyvaultplugin.credentials.string.AzureSecretStringCredentials,
         Jenkins.instance
     ).collectEntries{[(it.id.replaceAll('-', '_')): it.secret.plainText]}
+
+    com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        org.jenkinsci.plugins.azurekeyvaultplugin.credentials.usernamepassword.AzureUsernamePasswordCredentials,
+        Jenkins.instance
+    ).each {
+        String id = it.id.replace('-', '_')
+        secrets["${id}_USERNAME"] = it.username
+        secrets["${id}_PASSWORD"] = it.password
+    }
+
+    return secrets
 }
