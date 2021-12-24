44789e402259e129269e61d888ced73fd1c41a22
Mike Fugate <mfugate@carnegielearning.com>
2021-12-23 19:54:34 -0500

    switch to a separate jenkinsfile for update secrets job

--- jobs/Update-Secrets/jobConfig.groovy
+++ jobs/Update-Secrets/jobConfig.groovy
@@ -1,27 +1,16 @@
-String jenkinsfile = '''\
-if (params.updatedSecrets) {
-
-    // Can't use join directly on the result from readJSON, but collect
-    // converts it to a normal list that we can use join on
-    String secrets = readJSON(text: params.updatedSecrets).collect{it}.join(',').replace('-', '_')
-
-    build (
-        job: 'Update-Home-Assistant-Config/main',
-        wait: false,
-        parameters: [
-            string(name: 'updatedSecrets', value: secrets)
-        ]
-    )
-} else {
-    echo 'No secrets to update'
-}
-'''
-
-
 pipelineJob ('Update-Secrets') {
     definition {
-        cps {
-            script(jenkinsfile)
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/mfugate1/jenkins.git')
+                    }
+                    branch('main')
+                }
+                lightweight()
+                scriptPath('jobs/Update-Secrets/Jenkinsfile')
+            }
         }
     }
     properties {
