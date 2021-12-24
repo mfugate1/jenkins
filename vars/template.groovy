b9f415f03fd60512b761cd8a579b2a69d8f1fe95
Mike Fugate <mfugate@carnegielearning.com>
2021-12-23 20:04:08 -0500

    cleanup unused functions

--- vars/template.groovy
+++
@@ -1,9 +0,0 @@
-@NonCPS
-String call(String text, Map templateMap) {
-    extractVariablesFromString(text).findAll{!templateMap.containsKey(it)}.each {
-        templateMap[it] = '${' + it + '}'
-    }
-    return new groovy.text.GStringTemplateEngine().createTemplate(text)
-                                                  .make(templateMap)
-                                                  .toString()
-}
